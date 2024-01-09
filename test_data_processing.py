a7669ee299d8f9fda9d5f77ac233faef067d513c
Anne-Marie <annemarie.tousch@datadoghq.com>
2024-01-09 21:41:50 +0000

    Add first test!

---
+++ test_data_processing.py
@@ -0,0 +1,12 @@
+import unittest
+
+from data_processing import autocorrect_phone
+
+
+class TestDataProcessing(unittest.TestCase):
+    def test_autocorrect_phone(self):
+        output = autocorrect_phone("123456789.0")
+        self.assertEqual(output, "01 23 45 67 89")
+
+        output = autocorrect_phone("623456780.0")
+        self.assertEqual(output, "06 23 45 67 80")
